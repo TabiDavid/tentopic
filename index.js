8ccb94126d9aecaa7c5545eb96c514bb506295a2
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-11-15 04:58:37 -0500

    commit

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
+
+function makeBig() { 
+    var Video = document.getElementById("japanese");
+    Video.width = 560; 
+} 
+
 function Write(){
     var c = document.getElementById("canvas1");
 var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
 ctx.fillText("Canvas is canvas!",10,50);
 };
-
-var myVideo = document.getElementById("japanese");
-
-function Playvideo() { 
-  if (myVideo.paused)
-
-  myVideo.play();
-  else 
-    myVideo.pause();
-}
